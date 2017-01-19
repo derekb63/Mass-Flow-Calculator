9f0168aa7591c7e81a490f9ed4ac2b2fc3a39062
Derek Bean <derekrbean@gmail.com>
2017-01-19 11:11:51 -0800

    the solver appears to be working with hard coded numbers for mole. Next step is to add the mole fraction based on fuel oxidizer and equivalence ratio to make a more versitale code

--- gamma_match_solver.py
+++ gamma_match_solver.py
@@ -8,6 +8,8 @@ Created on Wed Jan 18 16:10:15 2017
 from sympy.solvers import solve
 from sympy import Symbol
 import cantera as ct
+import numpy as np
+import sys
 
 
 def gamma_finder(comp):
@@ -19,13 +21,28 @@ def gamma_finder(comp):
     return k
 
 phi = 1
-elements = ['C3H8', 'N2O', 'N2', 'CO2']
-coeffs = [1, 10*phi, 1, None]
+N2_dilution = 1
+fuel = 'C3H8'
+oxidizer = 'N2O'
+known_diluent = 'N2'
+unknown_diluent = 'CO2'
+elements = [fuel, oxidizer, known_diluent, unknown_diluent]
+if len(set(elements)) != len(elements):
+    sys.exit('There are duplicates in the elments list.' +
+             'This is not allowed' +
+             'Please eliminate the duplicate and try again')
+coeffs = [1, 10*phi, N2_dilution, None]
 
 ct_comp = [x + ':1' for x in elements]
 k = [gamma_finder(x) for x in ct_comp]
 props = dict(zip(elements, zip(k, coeffs)))
 
-N_CO2 = Symbol('N_CO2')
+X = Symbol(elements[-1])
 
-solve(1/(sum(coeffs)))*(N_C3H8*gamma['C3H8']+N_N2O*gamma[']), N_CO2)
+a = solve(1/(sum(coeffs[0:-1])) *
+          sum([np.prod(props[x]) for x in elements[0:-1]]) -
+             1/(sum(coeffs[0:2]) + X) *
+               (sum([np.prod(props[x]) for x in elements[0:2]]) +
+                X*props[elements[-1]][0]), X)
+a = a[0]
+print(a)
