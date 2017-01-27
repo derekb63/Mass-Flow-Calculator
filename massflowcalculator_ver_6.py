743f89701ea828dd33ee367698bfc6e9c3839a4d
Derek Bean <derekrbean@gmail.com>
2017-01-26 17:55:29 -0800

    Cleaned up mass flow calculator by removing unecessary variables and adding better comments. The equivalence ratio calculator needs changed to work with different fuels and the functions file needs cleaned up

--- massflowcalculator_ver_6.py
+++ massflowcalculator_ver_6.py
@@ -6,27 +6,17 @@ Created on Wed Nov  9 19:32:20 2016
 @author: beande
 """
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-import cantera as ct
 from nptdms import TdmsFile
-from tkinter import *
-from tkinter.filedialog import askopenfilename
 import time
 from functions import reformat, find_M_dot, velocity_calc, FindFile
 
 
-def mass_flow_calc(diluent, Tname=None, Pname=None, PDname=None):
+def mass_flow_calc(fuel='C3H8', oxidizer='N2O', diluent=None,
+                   Tname=None, Pname=None, PDname=None):
     start = time.time()
 
-    # Constants
-
-    # diluent = 'CO2'
-    fuel = 'C3H8'
-    oxidizer = 'N2O'
     Gases = [oxidizer, fuel, diluent]  # Species of the gas used ct form
-    R = ct.gas_constant / 1000       # Gas constant (kPa m^3/kmol-K)
-    P_d = 101325                     # Downstream pressure in kPa
 
     """ If you don't want to choose the files """
     # Tname  = 'January20\TC.tdms'
@@ -42,25 +32,20 @@ def mass_flow_calc(diluent, Tname=None, Pname=None, PDname=None):
             [15671, -62.616],
             [15667, -62.535],
             [15642, -62.406]]
-    # The number of transducers and thermocouples read in the tdms file
-    # numPT = 8
-    # numTC = 3
-    numPT = 8
-    numTC = 3
-    gas = ct.Solution('gri30.xml')
 
-
-    # Import Temperature File
-    if Tname == None:
+    # Get the filepaths for the termperature, pressure and photodiode data if
+    # they are not given in the function call
+    if Tname is None:
         Tname = FindFile('Temperature Open')
-    if Pname == None:
+
+    if Pname is None:
         Pname = FindFile('Pressure Open')
-    if PDname == None:
+
+    if PDname is None:
         PDname = FindFile('Photodiode Open')
 
-
     ################################################
-
+    # Import the TDMS files corresponding to the filepaths
     Pressfile = TdmsFile(Pname)
     Pressdata = Pressfile.as_dataframe(time_index=True, absolute_time=False)
     Pressdata = reformat(Pressdata)
@@ -68,30 +53,14 @@ def mass_flow_calc(diluent, Tname=None, Pname=None, PDname=None):
     Tempfile = TdmsFile(Tname)
     Tempdata = Tempfile.as_dataframe(time_index=True, absolute_time=False)
     Tempdata = reformat(Tempdata)
+
     numTests = len(Tempdata)
     ##############################################################
-
-    ##############################################################
-    # Example to run to find m_dot
-    '''
-    TC=1
-    D_orifice=0.047 ##Diameter of the orifice in INCHES
-    Gas='C3H8'
-    test=1
-    ducer=6
-    m_dot=find_M_dot(Tempdata,Pressdata,test, ducer, TC, D_orifice, cals, Gas)
-    print(type(m_dot))
-    print(Gas,test)
-    print(m_dot)
-    print()
-
-    '''
 
     # Initialize M_dot
     M_dot = pd.DataFrame(index=range(0, numTests), columns=Gases)
 
     for Gas in Gases:
-
         if (Gas == 'Propane') or (Gas == 'C3H8'):
             ducer = 6
             TC = 1
@@ -121,8 +90,7 @@ def mass_flow_calc(diluent, Tname=None, Pname=None, PDname=None):
     # Equivelance Ratio
     phi = 10*np.divide(M_dot[fuel], M_dot[oxidizer]).rename('Phi')
 
-    print()
-    # Mass Dilution Ratio
+    # Mass Fraction of Diluent
     dilution = np.divide(M_dot[diluent],
                          M_dot[fuel] + M_dot[oxidizer] +
                          M_dot[diluent]).rename('Diluent'+' ('+diluent+')')
@@ -144,10 +112,10 @@ def mass_flow_calc(diluent, Tname=None, Pname=None, PDname=None):
     # Write the data file to the same location as the photodiode data was
     # sourced by creating a new file or appending to the file
 
-
     Data.to_csv('/'.join(PDname.split('/')[:-1]) + 'testdata.csv', mode='a')
 
     print(Data)
+    Data.plot(x='Phi', y=['V1', 'V2', 'V3'], marker='x', linestyle='None')
 
     end = time.time()
     print('Run Time:', end-start, 'seconds')
@@ -155,4 +123,8 @@ def mass_flow_calc(diluent, Tname=None, Pname=None, PDname=None):
     return Data
 
 if __name__ == '__main__':
-    Data = mass_flow_calc(diluent = 'N2')
+
+    Data = mass_flow_calc(diluent='N2',
+                          Tname='E:/PDE Project/No Dilution/TC.tdms',
+                          Pname='E:/PDE Project/No Dilution/PT.tdms',
+                          PDname='E:/PDE Project/No Dilution/PD.tdms')
